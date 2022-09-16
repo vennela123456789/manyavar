305330a551fb739ea3e00250ee940fd2eeab80f7
Vennela B <vennela@devteam.ida>
2022-09-16 18:11:43 +0530

    first commit

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -12,125 +12,160 @@ import { ContactUsService } from '../services/contact-us.service';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-public loginForm!:FormGroup;
-public code:number=0;
-public details:any;
-public submitted:boolean=false;
-public radioVisible:boolean=false;
-isCaptchaMissMatched :boolean=false;
-orderVisible:boolean=false;
-storeVisible:boolean=false;
-random:any;
+  public loginForm!: FormGroup;
+  public code: number = 0;
+  public details: any;
+  public submitted: boolean = false;
+  public radioVisible: boolean = false;
+  isCaptchaMissMatched: boolean = false;
+  orderVisible: boolean = false;
+  storeVisible: boolean = false;
+  random: any;
 
-  constructor(private router:Router,private fb:FormBuilder,private http:HttpClient,private contact:ContactUsService) { }
+  constructor(private router: Router, private fb: FormBuilder, private http: HttpClient, private contact: ContactUsService) { }
 
   ngOnInit(): void {
-    this.loginForm=this.fb.group({
-      selectedSubject:[''],
-      selectType:[''],
-      orderNumber:[''],
-      StoreCity:[''],
-      StoreName:[''],
-      Name:['',[Validators.required,Validators.minLength(3),Validators.pattern('^[a-zA-Z ]*$')]],
-      City:['',Validators.required],
-      
-      Email:['',[Validators.required,Validators.email]],
-      Mobile:['',[Validators.required,Validators.pattern(/^[6789][0-9]{9}$/)]],
-      Feedback:['',Validators.required],
-      Captcha:['',Validators.required],
+    this.loginForm = this.fb.group({
+      selectedSubject: [''],
+      selectType: [''],
+      orderNumber: [''],
+      StoreCity: [''],
+      StoreName: [''],
+      Name: ['', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z ]*$')]],
+      City: ['', Validators.required],
+
+      Email: ['', [Validators.required, Validators.email]],
+      Mobile: ['', [Validators.required, Validators.pattern(/^[6789][0-9]{9}$/)]],
+      Feedback: ['', Validators.required],
+      Captcha: ['', [Validators.required, Validators.minLength(6)]]
     })
     // this.code=this.captcha.GenerateCode();
     //this.canvasGenerate(this.code)
     this.newCode();
   }
- 
-login(){
 
-}
- newCode(){
-  let random = Math.floor(Math.random() * (999999 - 100000)) + 100000
-  // this.code=this.captcha.GenerateCode();
-  this.random=random;
-  this.canvasGenerate(random)
-}
-userDetails(){
-  debugger
- this.submitted=true;
- if(this.loginForm.invalid){
-  return
- }
+  login() {
 
- else{
-
-
- 
-  let req={
-    CaptchaCode:this.loginForm.value.Captcha,
-    City:  this.loginForm.value.City,
-    CurrencyId: "1",
-    CustomerToken: "",
-    Email: this.loginForm.value.Email,
-    Enquiry: this.loginForm.value.Feedback,
-    FirstName: this.loginForm.value.Name,
-    IsOnlineOrder:  this.loginForm.value.selectType,
-    MobileCountryCode : 1,
-    Phone :   this.loginForm.value.Mobile,
-    StoreCity :  this.loginForm.value.storeCity,
-    StoreName : this.loginForm.value.storeName,
-    Subject: this.loginForm.value.selectedSubject,
-    OrderNumber:this.loginForm.value.orderNumber
-
-
-
-
-
-   
   }
-  this.contact.postService(ContactUs,req).subscribe(data=>this.details=data)
- }
-  
-}
+  newCode() {
+    let random = Math.floor(Math.random() * (999999 - 100000)) + 100000
+    // this.code=this.captcha.GenerateCode();
+    this.random = random;
+    this.canvasGenerate(random)
+  }
+  userDetails() {
+    debugger
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return
+    }
 
-radiolist(event:any){
- if(event.target.value=='Customer Feedback' || event.target.value=='Returns & Exchange' || event.target.value=='Payments'){
-  this.radioVisible=true;
- }
-else{
-  this.radioVisible=false;
-}
-}
-orderNumber(event:any){
-  if(event.target.value=='true'){
-    this.orderVisible=true;
-    this.storeVisible=false;
+    else {
+
+
+      let req: FormData = new FormData;
+      req.CaptchaCode = this.loginForm.value.Captcha;
+      req.City = this.loginForm.value.City;
+
+      req.Email = this.loginForm.value.Email;
+      req.Enquiry = this.loginForm.value.Feedback;
+      req.FirstName = this.loginForm.value.Name;
+
+
+      req.Phone = this.loginForm.value.Mobile;
+
+      req.Subject = this.loginForm.value.selectedSubject;
+
+      if (this.orderVisible == true) {
+        req.IsOnlineOrder = true;
+        req.OrderNumber = this.loginForm.value.orderNumber;
+
+      }
+      else if (this.storeVisible == true) {
+        req.IsOnlineOrder = false;
+        req.StoreName = this.loginForm.value.storeName;
+        req.StoreCity = this.loginForm.value.storeCity;
+      }
+      else {
+        req.OrderNumber = '';
+        req.StoreCity = '';
+        req.StoreName = ''
+      }
+
+      //   let req={
+      //     CaptchaCode:this.loginForm.value.Captcha,
+      //     City:  this.loginForm.value.City,
+      //     CurrencyId: "1",
+      //     CustomerToken: "",
+      //     Email: this.loginForm.value.Email,
+      //     Enquiry: this.loginForm.value.Feedback,
+      //     FirstName: this.loginForm.value.Name,
+      //     IsOnlineOrder:  this.loginForm.value.selectType,
+      //     MobileCountryCode : 1,
+      //     Phone :   this.loginForm.value.Mobile,
+      //     StoreCity :  this.loginForm.value.storeCity,
+      //     StoreName : this.loginForm.value.storeName,
+      //     Subject: this.loginForm.value.selectedSubject,
+      //     OrderNumber:this.loginForm.value.orderNumber
+      // }
+      this.contact.postService(ContactUs, req).subscribe(data => this.details = data)
+    }
+
   }
-}
-storeName(e:any){
-  if(e.target.value=='false'){
-    this.storeVisible=true;
-    this.orderVisible=false;
+
+  radiolist(event: any) {
+    if (event.target.value == 'Customer Feedback' || event.target.value == 'Returns & Exchange' || event.target.value == 'Payments') {
+      this.radioVisible = true;
+    }
+    else {
+      this.radioVisible = false;
+    }
+
   }
-}
-canvasGenerate(code:any){
-  var c =<HTMLCanvasElement> document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-ctx?.clearRect(0,0,c.height,c.width);
-ctx!.font="20px Arial"
-  ctx?.fillText(code,10,50);
-}
-validateCaptcha(e:any){
-  if(e.target.value==this.random){
-this.isCaptchaMissMatched=false;
+  orderNumber(event: any) {
+    if (event.target.value == 'true') {
+      this.orderVisible = true;
+      this.storeVisible = false;
+    }
   }
-  else{
-this.isCaptchaMissMatched=true;
+  storeName(e: any) {
+    if (e.target.value == 'false') {
+      this.storeVisible = true;
+      this.orderVisible = false;
+    }
   }
-}
+  canvasGenerate(code: any) {
+    var c = <HTMLCanvasElement>document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx?.clearRect(0, 0, c.height, c.width);
+    ctx!.font = "20px Arial"
+    ctx?.fillText(code, 10, 50);
+  }
+  validateCaptcha(e: any) {
+    if (e.target.value == this.random) {
+      this.isCaptchaMissMatched = false;
+    }
+    else {
+      this.isCaptchaMissMatched = true;
+    }
+  }
 }
 
 export class FormData {
-  OrderNumber:string ='';
-  StoreName :string = '';
-  StoreCity :string ='';
+  CaptchaCode!: number;
+  City!: string;
+  CurrencyId: string = "1";
+  CustomerToken: string = "";
+  Email!: string;
+  Enquiry!: string;
+  FirstName!: string;
+  IsOnlineOrder!: boolean;
+  MobileCountryCode: number = 1;
+  Phone!: number;
+  StoreCity!: string;
+  StoreName!: string;
+  Subject!: string;
+  OrderNumber!: string;
+
 
 }
